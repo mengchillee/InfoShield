11a483f627ed198d0052850211087d119d68634a
mengchillee <jeremy08300830@gmail.com>
2020-08-06 17:01:45 +0800

    Update seqgraphalignment.py

--- seqgraphalignment.py
+++ seqgraphalignment.py
@@ -1,3 +1,8 @@
+####################################
+# Author: Jeremy (Meng-Chieh) Lee  #
+# Date	: 2020/06/02               #
+####################################
+
 import numpy as np
 from math import ceil
 from collections import Counter
